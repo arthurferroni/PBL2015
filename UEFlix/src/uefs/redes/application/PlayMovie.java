5d1fad91b04c21e3570bd4869cd269952d794a34
jeyelferreira <jeyelferreira@gmail.com>
2015-03-31 13:55:30 -0300

    Correções 1

--- UEFlix/src/uefs/redes/application/PlayMovie.java
+++ UEFlix/src/uefs/redes/application/PlayMovie.java
@@ -3,6 +3,9 @@ package uefs.redes.application;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+
+import javafx.embed.swing.JFXPanel;
 
 import javax.swing.JButton;
 import javax.swing.JComponent;
@@ -10,44 +13,11 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
 import org.jfxtras.scene.SceneToJComponent;
 
 public class PlayMovie extends JFrame {
 	
-	public static JTextField tf = new JTextField("JavaFX for SWING");
-	
-	public PlayMovie() {
-		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);  
-	    setTitle("JavaFX in SWING Test");  
-	  
-	    Container container = getContentPane();  
-	    container.setLayout(new BorderLayout());  
-	  
-	    String sceneClass = "uefs.redes.application.MovieScreen";  
-	    JComponent myScene = SceneToJComponent.loadScene(sceneClass);  
-	  
-	    JLabel label = new JLabel(" Below is a JavaFX Animation: ");  
-	    container.add(label, BorderLayout.NORTH);  
-	    container.add(myScene, BorderLayout.CENTER);  
-	  
-	    JPanel p = new JPanel();  
-	    p.setLayout(new FlowLayout());  
-	  
-	    tf.setColumns(28);  
-	    p.add(tf);  
-	    p.add(new JButton("SWING Button"));  
-	  
-	    container.add(p, BorderLayout.SOUTH);  
-	    pack();  
-	}
-	
-	public static void main(String args[]) {  
-	    java.awt.EventQueue.invokeLater(  
-	      new Runnable() {  
-	        public void run() {  
-	          new PlayMovie().setVisible(true);  
-	        }  
-	      });  
-	  }  
+
 
 }
