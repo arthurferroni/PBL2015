c1080384cef9214cacf3a5090b5a8413a74aa3b0
arthurferroni <arthur.ferroni@live.com>
2015-04-04 23:30:22 -0300

    Revert "A porra toda FUNCIONANDO só falta o teu bagui mulek"
    This reverts commit 997e155d7471ad7fa2baa778a0259fd44beb1c47.
    
    Conflicts:
    	UEFlix/src/uefs/redes/application/Catalogo.java
    	UEFlix/src/uefs/redes/application/Historico.java
    	UEFlix/src/uefs/redes/application/Login.java
    	UEFlix/src/uefs/redes/application/MovieDesc.java
    	UEFlix/src/uefs/redes/application/PlayMovie.java
    	UEFlix/src/uefs/redes/application/Register.java
    	UEFlix/src/uefs/redes/controller/ControllerClient.java
    	UEFlix/src/uefs/redes/controller/ControllerServers.java
    	UEFlix/src/uefs/redes/controller/videos.java
    	UEFlix/src/uefs/redes/model/ClientAcess.java
    	UEFlix/src/uefs/redes/model/ClientInformation.java
    	UEFlix/src/uefs/redes/servidorAcesso/ServerAcess.java

--- UEFlix/src/uefs/redes/model/ClientAcess.java
+++ UEFlix/src/uefs/redes/model/ClientAcess.java
@@ -1,3 +1,4 @@
+<<<<<<< OURS
 package uefs.redes.model;
 
 import java.io.IOException;
@@ -235,3 +236,166 @@ public class ClientAcess implements Runnable {
 	
 
 }
+=======
+package uefs.redes.model;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+import uefs.redes.controller.ControllerClient;
+import uefs.redes.define.Constants;
+import uefs.redes.define.Pack;
+
+public class ClientAcess implements Runnable {
+
+	
+	private Socket socket;
+	
+	private String name_client;
+	private String login_client;
+	private String pass_client;
+	private int en_req;
+	private ControllerClient controll_client = null;
+	private ArrayList<MovieInformation>history = new  ArrayList<MovieInformation>();
+	private ArrayList<MovieInformation>search = new ArrayList<MovieInformation>();
+	
+	public ClientAcess(Socket sock, ControllerClient cCLient) throws IOException
+	{
+		socket = sock;	
+		controll_client = cCLient;
+	}
+	public ClientAcess(Socket sock) throws IOException
+	{
+		socket = sock;
+	}
+       
+	@SuppressWarnings("unused")
+	@Override
+	public void run() 											//	INHERIT THE RUN METHOD FROM THE Runnable INTERFACE
+	{
+		try
+		{
+			Pack pack_reqs  = null;
+			ObjectInputStream in = null;
+			ObjectOutputStream out = null;
+			String message;
+			do
+			{
+				in =  new  ObjectInputStream(socket.getInputStream());
+				 pack_reqs = (Pack) in.readObject();
+
+				switch(pack_reqs.getCode()) {
+				case Constants.LOGIN_REP:
+					ClientInformation client_info = (ClientInformation) pack_reqs.getInformation(0);
+					message = (String) pack_reqs.getInformation(2);
+					//System.out.println(message);
+                                        login_client = client_info.getLogin();
+                                        name_client = client_info.getName();
+                                        pass_client = client_info.getPassword();
+					// da a mensagem na interface.
+					ArrayList<MovieInformation> moviesInformation =  (ArrayList<MovieInformation>) pack_reqs.getInformation(1);
+					
+                                        controll_client.setMoviesInformation(moviesInformation);
+					// modifica para a tela de filmes 
+                                        en_req = Constants.LOGIN_REP;
+					break;
+				case Constants.LOGIN_RER:
+					
+					message = (String) pack_reqs.getInformation(0);
+					//System.out.println(message);
+					// apresenta a messagem e 
+					// muda para a interface de fazer login 
+                                       en_req = Constants.LOGIN_RER;
+					break;
+				case Constants.LOGOUT_REP:
+					
+					message = (String) pack_reqs.getInformation(0);
+					//System.out.println(message);
+                                        en_req = Constants.LOGOUT_REP;
+					// apresenta a messagem e 
+					// muda para a interface de fazer login 
+                                        socket.close();
+					break;
+				case Constants.REGISTER_REP:
+					message = (String) pack_reqs.getInformation(0);
+					//System.out.println(message);
+                                        en_req =Constants.REGISTER_REP;
+					// apresenta a messagem e 
+                                        
+					// muda para a interface de fazer login 
+					break;
+				case Constants.REGISTER_RER:
+					message = (String) pack_reqs.getInformation(0);
+					//System.out.println(message);
+                                        en_req =Constants.REGISTER_RER;
+					// apresenta a messagem e 
+					// muda para a interface de fazer Cadastro
+					
+					break;
+				case Constants.SEARCH_REP:
+					message = (String) pack_reqs.getInformation(0);
+					//System.out.println(message);
+					 search = (ArrayList<MovieInformation>) pack_reqs.getInformation(1);
+                                        // System.out.print(search.size());
+					 en_req = Constants.SEARCH_REP;
+                                        // apresenta a messagem e 
+					// muda para a interface apresentando a lista de filmes envontrados
+					break;
+				case Constants.SEARCH_RER:
+					message = (String) pack_reqs.getInformation(0);
+					System.out.println(message);
+                                         en_req = Constants.SEARCH_RER;
+					// apresenta a messagem e 
+					// muda para a interface de fazer Cadastro
+					break;
+				case Constants.DOWNLOAD_REP:
+					
+                                    history = (ArrayList<MovieInformation> )pack_reqs.getInformation(0);
+                              
+                                    
+                                    break;
+                                
+				default:
+                                   
+					break;
+					
+				
+				}
+				
+			}
+			while(true);
+			
+		}
+		catch (Exception e)
+		{
+			JOptionPane.showMessageDialog(null, "Caiu a conexao com o servidor. tente novamente mais tarde !" );
+		} 
+	}
+        public ArrayList<MovieInformation> getHistory()
+        {
+            return this.history;
+        }
+        public ArrayList<MovieInformation> getSearch()
+        {
+            return this.search;
+        }
+	public int req()
+        {
+            return en_req;
+        }
+        public String getLogin()
+        {
+            return login_client;
+        }
+        public Socket getSocket()
+        {
+            return socket;
+        }
+	
+
+}
+>>>>>>> THEIRS
--- UEFlix/src/uefs/redes/servidorAcesso/ServerAcess.java
+++ UEFlix/src/uefs/redes/servidorAcesso/ServerAcess.java
@@ -1,3 +1,4 @@
+<<<<<<< OURS
 package uefs.redes.servidorAcesso;
 <<<<<<< HEAD
 import java.io.File;
@@ -296,3 +297,219 @@ public class ServerAcess implements Runnable{
 =======
 >>>>>>> origin/master
 }
+=======
+package uefs.redes.servidorAcesso;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.time.Clock;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import uefs.redes.define.Constants;
+import uefs.redes.define.Pack;
+import uefs.redes.model.ClientInformation;
+import uefs.redes.model.MovieInformation;
+
+public class ServerAcess implements Runnable{
+
+	private ClientInformation client_information = null;
+	private Socket socket;
+	@SuppressWarnings("unused")
+	private ArrayList<MovieInformation> moviesInformation = new ArrayList<MovieInformation>();
+	private ArrayList<ClientInformation> clients = new  ArrayList<ClientInformation>();
+
+	public ServerAcess(Socket sock) throws IOException
+	{
+		this.socket = sock;
+	}
+	@Override
+	public void run()
+	{
+			
+		try	
+		{
+			Pack pack_reqs ;
+			ObjectInputStream in;
+			
+			do
+			{
+				String name;
+				String pass;
+				String login_name;
+				
+				boolean hasClient = false;
+					in =  new  ObjectInputStream(socket.getInputStream());
+					 pack_reqs = (Pack) in.readObject();
+	
+					switch(pack_reqs.getCode()) {
+					case Constants.LOGIN_REQ:
+						//System.out.println("login-REQ");
+						//
+						 name = (String)pack_reqs.getInformation(0);
+						 pass = (String)pack_reqs.getInformation(1);
+						
+						 
+						
+						for(ClientInformation xClient:this.clients)
+						{
+							if(xClient.getLogin().equals(name))
+							{
+								client_information = xClient;
+								hasClient = true;
+								break;
+							}
+						}
+						if(hasClient)
+						{
+							pack_reqs = new Pack(Constants.LOGIN_REP);
+							pack_reqs.addInformation(client_information);
+							pack_reqs.addInformation(moviesInformation);
+							pack_reqs.addInformation(Constants.MESSAGE_INFORMATION+"Login.");
+						}
+						else{
+							pack_reqs = new Pack(Constants.LOGIN_RER);
+							pack_reqs.addInformation(Constants.MESSAGE_ERROR+"Login verifique as informa��es.");
+						}
+						// CODIGO 
+						this.send_pack(pack_reqs);
+						break;
+					case Constants.LOGOUT_REQ:
+						
+						pack_reqs.setCode(Constants.LOGOUT_REP);
+						pack_reqs.addInformation(Constants.MESSAGE_INFORMATION+" Logout.");
+						this.send_pack(pack_reqs);
+                                                socket.close();
+						break;
+					case Constants.REGISTER_REQ:
+						 
+						 name = (String) pack_reqs.getInformation(0);
+						 login_name = (String) pack_reqs.getInformation(1);
+						 pass = (String) pack_reqs.getInformation(2);
+						 
+						  hasClient = false;
+						  ClientInformation clientInfor = null;
+						  
+						  for(ClientInformation xClient:this.clients)
+							{
+								if(xClient.getLogin().equals(login_name))
+								{
+									hasClient = true;
+									break;
+								}
+							}
+							if(!hasClient)
+							{
+								clientInfor = new ClientInformation(name,login_name,pass);
+								clients.add(clientInfor);
+								pack_reqs = new Pack(Constants.REGISTER_REP);
+								pack_reqs.addInformation(Constants.MESSAGE_INFORMATION+"Cadastro.");
+								
+							}
+							else{
+								
+								pack_reqs = new Pack(Constants.REGISTER_RER);
+								pack_reqs.addInformation(Constants.MESSAGE_ERROR+" Cadastrado Usuário já existe.");
+								
+								
+							}
+						  // CODIGO
+						 this.send_pack(pack_reqs);
+						 hasClient = false;
+						break;
+					case Constants.SEARCH_REQ:
+						String tag_movie;
+						tag_movie = (String)pack_reqs.getInformation(0);
+						//System.out.println(tag_movie);
+						
+						ArrayList<MovieInformation> movieslist = searchMovies(tag_movie);
+                                                
+							if(movieslist.isEmpty())
+							{
+								pack_reqs = new Pack(Constants.SEARCH_RER);
+								pack_reqs.addInformation(Constants.MESSAGE_ERROR+"Search n�o h� videos com essa tag");
+							}
+							else
+							{
+								pack_reqs = new Pack(Constants.SEARCH_REP);
+								pack_reqs.addInformation(Constants.MESSAGE_INFORMATION + "Search");
+								pack_reqs.addInformation(movieslist);
+							}
+							// CODIGO 
+							this.send_pack(pack_reqs);
+							
+						break;
+					case Constants.DOWNLOAD_REQ:
+						String tag_movie1;
+						
+						tag_movie1 = (String) pack_reqs.getInformation(0);
+						MovieInformation movieslists = getMovies(tag_movie1);
+						client_information.addMoiveHistory(movieslists);
+                                                pack_reqs.setCode(Constants.DOWNLOAD_REP);
+                                                pack_reqs.addInformation(client_information.getMyMovies());
+                                                this.send_pack(pack_reqs);
+						break;
+					default:
+						
+						break;
+						
+					
+					}
+							
+			}
+			while(true);
+			
+		} 
+		catch (Exception e)
+		{
+		System.out.println(client_information.getName()+" foi desconectado.");								
+		}	
+	}
+	private void send_pack(Pack pack) throws IOException
+	{
+		ObjectOutputStream out;
+		out = new ObjectOutputStream(socket.getOutputStream());
+		out.writeObject(pack);
+	}
+	
+	private ArrayList<MovieInformation> searchMovies(String tag_movie)
+	{
+		
+		ArrayList<MovieInformation> list = new ArrayList<MovieInformation>();
+		for(MovieInformation movies:this.moviesInformation)
+		{
+			ArrayList<String> x = movies.getTags_file();
+			for(String y:x)
+			{
+				if(y.equals(tag_movie))
+                                   list.add(movies);
+			}
+                      
+		}
+                
+		return list;
+	}
+	private MovieInformation getMovies(String tag_movie)
+	{
+		
+		MovieInformation list = null;
+		for(MovieInformation movies:this.moviesInformation)
+		{
+			String x = movies.getName_file();
+			
+				if(x.equals(tag_movie))
+					list = movies;
+		}
+                
+		return list;
+	}
+	
+	public void setDatas(ArrayList<ClientInformation> x, ArrayList<MovieInformation> y)
+	{
+		this.clients = x;
+		this.moviesInformation = y;
+	}
+}
+>>>>>>> THEIRS
